cbcce38c5291376267e2374a3fd4a5ee4135725d
Lobzev Nikita <lobzev.n@yandex.ru>
2024-11-01 00:15:32 +0300

    Реализовал логику поиска, осталось добавить удаление контакта

--- app/src/main/java/com/example/recyclerviewfinal/ContactsFragment.kt
+++ app/src/main/java/com/example/recyclerviewfinal/ContactsFragment.kt
@@ -1,9 +1,12 @@
 package com.example.recyclerviewfinal
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,11 +21,25 @@ private lateinit var fragmentDetails: ContactDetailsFragment
 class ContactsFragment : Fragment() {
 
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_contacts, container, false)
+        val searchEditText: EditText = view.findViewById(R.id.searchEditText)
+        searchEditText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                adapter.filter(s.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
         return view
 
 
